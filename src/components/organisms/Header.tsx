3e494d986eb863ef81632582bae40a21fa7f08c0
Yusuke <yhakamaya@adobe.com>
2022-11-07 11:54:22 +0900

    Cnovert named imports to namespace imports

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,16 +1,27 @@
 import { HamburgerIcon } from '@chakra-ui/icons';
-import * as ChakraUI from '@chakra-ui/react';
+import {
+  useDisclosure,
+  Flex,
+  Heading,
+  Box,
+  IconButton,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerBody,
+  Button,
+} from '@chakra-ui/react';
 import { FC, memo } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header: FC = memo(() => {
-  const { isOpen, onOpen, onClose } = ChakraUI.useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
-      <ChakraUI.Flex as="nav" align="center" justify="space-between" padding={{ base: 3, md: 5 }}>
-        <ChakraUI.Flex align="center" as="a" mr="8" _hover={{ cursor: 'pointer' }}>
-          <ChakraUI.Heading
+      <Flex as="nav" align="center" justify="space-between" padding={{ base: 3, md: 5 }}>
+        <Flex align="center" as="a" mr="8" _hover={{ cursor: 'pointer' }}>
+          <Heading
             as="h1"
             fontSize={{
               base: 'md',
@@ -18,9 +29,9 @@ const Header: FC = memo(() => {
             }}
           >
             Header
-          </ChakraUI.Heading>
-        </ChakraUI.Flex>
-        <ChakraUI.Flex
+          </Heading>
+        </Flex>
+        <Flex
           align="center"
           fontSize="sm"
           display={{
@@ -28,14 +39,14 @@ const Header: FC = memo(() => {
             md: 'flex',
           }}
         >
-          <ChakraUI.Box pr={4}>
+          <Box pr={4}>
             <Link to="/users">Users</Link>
-          </ChakraUI.Box>
-          <ChakraUI.Box pr={0}>
+          </Box>
+          <Box pr={0}>
             <Link to="/settings">Settings</Link>
-          </ChakraUI.Box>
-        </ChakraUI.Flex>
-        <ChakraUI.IconButton
+          </Box>
+        </Flex>
+        <IconButton
           onClick={onOpen}
           aria-label="hamburger icon"
           icon={<HamburgerIcon />}
@@ -43,17 +54,17 @@ const Header: FC = memo(() => {
           variant="outline"
           display={{ base: 'block', md: 'none' }}
         />
-      </ChakraUI.Flex>
-      <ChakraUI.Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
-        <ChakraUI.DrawerOverlay>
-          <ChakraUI.DrawerContent>
-            <ChakraUI.DrawerBody p={0}>
-              <ChakraUI.Button w="100%">Users</ChakraUI.Button>
-              <ChakraUI.Button w="100%">Settings</ChakraUI.Button>
-            </ChakraUI.DrawerBody>
-          </ChakraUI.DrawerContent>
-        </ChakraUI.DrawerOverlay>
-      </ChakraUI.Drawer>
+      </Flex>
+      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerBody p={0}>
+              <Button w="100%">Users</Button>
+              <Button w="100%">Settings</Button>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
     </>
   );
 });
