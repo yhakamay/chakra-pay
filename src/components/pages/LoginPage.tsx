3b861e9056d145b6edaa092d3f7a560e79b710a2
Yusuke <yhakamaya@adobe.com>
2022-11-12 09:26:06 +0900

    WIP

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,18 +1,26 @@
 import {
   Button, Divider, Flex, Heading, Input,
 } from '@chakra-ui/react';
-import { FC, memo } from 'react';
+import { FC, memo, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 import ChakraPayCard from '../atoms/ChakraPayCard';
 
-const LoginPage: FC = memo(() => (
-  <Flex align="center" justify="center" height="100vh">
-    <ChakraPayCard>
-      <Heading as="h1" size="lg" textAlign="center">Chakra Pay</Heading>
-      <Divider my={4} />
-      <Input placeholder="Email" />
-      <Button w="100%" colorScheme="teal" variant="solid" my={4}>Login</Button>
-    </ChakraPayCard>
-  </Flex>
-));
+const LoginPage: FC = memo(() => {
+  const [email, setEmail] = useState('');
+  const { login, isLoading } = useAuth();
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
+  const onClickLogin = () => login(email);
+
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <ChakraPayCard>
+        <Heading as="h1" size="lg" textAlign="center">Chakra Pay</Heading>
+        <Divider my={4} />
+        <Input placeholder="Email" type="email" value={email} onChange={onChangeEmail} />
+        <Button onClick={onClickLogin} w="100%" colorScheme="teal" variant="solid" my={4} isLoading={isLoading} disabled={isLoading}>Login</Button>
+      </ChakraPayCard>
+    </Flex>
+  );
+});
 
 export default LoginPage;
